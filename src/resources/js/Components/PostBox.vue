40f23570ee518e951b5157ec2d578f77a2f9c0ae
juborajnaofel <juborajnaofel@gmail.com>
2023-06-14 00:56:17 +0600

    postbox width fix

--- src/resources/js/Components/PostBox.vue
+++ src/resources/js/Components/PostBox.vue
@@ -7,7 +7,7 @@ const form = useForm({
 </script>
 
 <template>
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="w-full mx-auto sm:px-6 lg:px-8">
         <div
             class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
         >
