a9775bcb98091f74ce25a5c67fb11fc7a0ff94fa
juborajnaofel <juborajnaofel@gmail.com>
2023-06-13 23:52:05 +0600

    post card component created

---
+++ src/resources/js/Components/PostCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    data: Object,
+});
+</script>
+
+<template>
+    <div class="max-w-full my-4 mx-auto sm:px-6 lg:px-8">
+        <div
+            class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
+        >
+            <div class="p-5 text-gray-900 dark:text-gray-100">
+                <h1>Name {{ data }} Sample</h1>
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptates blanditiis dignissimos magnam adipisci amet ut
+                incidunt, corrupti, ad ipsam esse saepe deserunt ex minima eum
+                id dolore dolores reprehenderit neque.
+            </div>
+        </div>
+    </div>
+</template>
