502205ba13fc1ec3bda6d84b6e5c087647b44e1d
juborajnaofel <juborajnaofel@gmail.com>
2023-06-14 00:59:20 +0600

    dummy data for PostCard in Homefeed page

--- src/resources/js/Pages/Homefeed.vue
+++ src/resources/js/Pages/Homefeed.vue
@@ -3,6 +3,75 @@ import PostBox from "@/Components/PostBox.vue";
 import PostCard from "@/Components/PostCard.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+const posts = ref([
+    {
+        userInfo: {
+            name: "John Doe",
+            link: "",
+            is_verified: true,
+        },
+        postInfo: {
+            content: "Hello, world!",
+            like_count: 10,
+            comment_count: 5,
+            share_count: 2,
+        },
+    },
+    {
+        userInfo: {
+            name: "Jane Smith",
+            link: "",
+            is_verified: false,
+        },
+        postInfo: {
+            content: "Check out this amazing recipe!",
+            like_count: 15,
+            comment_count: 7,
+            share_count: 3,
+        },
+    },
+    {
+        userInfo: {
+            name: "Michael Johnson",
+            link: "",
+            is_verified: true,
+        },
+        postInfo: {
+            content: "Excited for my upcoming vacation!",
+            like_count: 20,
+            comment_count: 10,
+            share_count: 4,
+        },
+    },
+    {
+        userInfo: {
+            name: "Emily Davis",
+            link: "",
+            is_verified: false,
+        },
+        postInfo: {
+            content: "Just finished reading a great book!",
+            like_count: 8,
+            comment_count: 3,
+            share_count: 1,
+        },
+    },
+    {
+        userInfo: {
+            name: "Alex Johnson",
+            link: "",
+            is_verified: true,
+        },
+        postInfo: {
+            content: "Sharing my latest photography work!",
+            like_count: 12,
+            comment_count: 6,
+            share_count: 2,
+        },
+    },
+]);
 </script>
 
 <template>
@@ -23,16 +92,14 @@ import { Head } from "@inertiajs/vue3";
                     <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
                 </div>
             </div> -->
-            <div class="flex flex-col md:flex-row max-w-7xl">
+            <div class="flex flex-col md:flex-row w-full px-5">
                 <div class="w-full md:w-1/5">This is for sidebar</div>
                 <div class="w-full md:w-4/5">
                     <PostBox />
                     <PostCard
-                        v-for="(i, index) in [
-                            1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 0, 0, 0,
-                        ]"
+                        v-for="(post, index) in posts"
                         :key="index"
-                        :data="i"
+                        :data="post"
                     />
                 </div>
             </div>
