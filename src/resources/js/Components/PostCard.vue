b9e64a20e071b41189533fe0ed8887974dc60015
juborajnaofel <juborajnaofel@gmail.com>
2023-06-14 00:58:29 +0600

    PostCard component data and design

--- src/resources/js/Components/PostCard.vue
+++ src/resources/js/Components/PostCard.vue
@@ -10,12 +10,17 @@ const props = defineProps({
             class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
         >
             <div class="p-5 text-gray-900 dark:text-gray-100">
-                <h1>Name {{ data }} Sample</h1>
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates blanditiis dignissimos magnam adipisci amet ut
-                incidunt, corrupti, ad ipsam esse saepe deserunt ex minima eum
-                id dolore dolores reprehenderit neque.
+                <span class="font-bold">{{ data.userInfo.name }} </span>
+                <span class="text-xs p-2" v-if="data.userInfo.is_verified">✔</span>
+                <p class="py-5">
+                    {{ data.postInfo.content }}
+                </p>
+                <hr>
+                <div class="flex flex-row flex-wrap gap-3 pt-3">
+                    <button class="text-xs py-1 px-6 rounded-md bg-slate-300 text-slate-600 font-bold">Likes: {{ data.postInfo.like_count }}</button>
+                    <button class="text-xs py-1 px-6 rounded-md bg-slate-300 text-slate-600 font-bold">Comments: {{ data.postInfo.comment_count }}</button>
+                    <button class="text-xs py-1 px-6 rounded-md bg-slate-300 text-slate-600 font-bold">Share: {{ data.postInfo.share_count }}</button>
+                </div>
             </div>
         </div>
     </div>
