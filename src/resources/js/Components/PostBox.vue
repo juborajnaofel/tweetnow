979a0b2b5df0281215dc2dbffeb5f47b45bf17d9
juborajnaofel <juborajnaofel@gmail.com>
2023-06-13 23:17:46 +0600

    post box component created

---
+++ src/resources/js/Components/PostBox.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    post_field: "",
+});
+</script>
+
+<template>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div
+            class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
+        >
+            <div class="p-1 text-gray-900 dark:text-gray-100">
+                <form action="" method="post" class="w-full">
+                    <textarea
+                        @change=""
+                        name="posthere"
+                        placeholder="What are you thinking?"
+                        v-model="form.post_field"
+                        rows="5"
+                        class="w-full rounded-md border-2 border-slate-100"
+                    ></textarea>
+                    <div class="flex flex-row justify-end">
+                        <button
+                            class="py-3 px-6 rounded-md bg-slate-300 text-slate-600 font-bold"
+                        >
+                            Post
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
--- src/resources/js/Pages/Dashboard.vue
+++ src/resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import PostBox from "@/Components/PostBox.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
@@ -8,15 +9,20 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
+            <h2
+                class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
+            >
+                Dashboard
+            </h2>
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
                 </div>
-            </div>
+            </div> -->
+            <PostBox />
         </div>
     </AuthenticatedLayout>
 </template>
