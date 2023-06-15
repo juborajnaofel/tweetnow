14dce85fc8f419c7898c7a725d46d6101ce3a53b
juborajnaofel <juborajnaofel@gmail.com>
2023-06-16 05:58:23 +0600

    PostCard component logic refactor and fix

--- src/resources/js/Components/PostCard.vue
+++ src/resources/js/Components/PostCard.vue
@@ -1,4 +1,14 @@
 <script setup>
+import TNowButton from "@/Components/TNButton.vue";
+import moment from "moment";
+const formattedDate = (datatime) => {
+    return moment(datatime).format("MMMM Do YYYY, h:mm:ss a");
+};
+
+const randomInt = (limit) => {
+    return Math.floor(Math.random() * limit) + 1;
+};
+
 const props = defineProps({
     data: Object,
 });
@@ -10,16 +20,17 @@ const props = defineProps({
             class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
         >
             <div class="p-5 text-gray-900 dark:text-gray-100">
-                <span class="font-bold">{{ data.userInfo.name }} </span>
-                <span class="text-xs p-2" v-if="data.userInfo.is_verified">✔</span>
+                <span class="font-bold">{{ data.user.name }}</span>
+                <br /><span>{{ formattedDate(data.created_at) }}</span>
+                <!-- <span class="text-xs p-2" v-if="data.user.name">✔</span> -->
                 <p class="py-5">
-                    {{ data.postInfo.content }}
+                    {{ data.content }}
                 </p>
-                <hr>
+                <hr />
                 <div class="flex flex-row flex-wrap gap-3 pt-3">
-                    <button class="text-xs py-1 px-6 rounded-md bg-slate-300 text-slate-600 font-bold">Likes: {{ data.postInfo.like_count }}</button>
-                    <button class="text-xs py-1 px-6 rounded-md bg-slate-300 text-slate-600 font-bold">Comments: {{ data.postInfo.comment_count }}</button>
-                    <button class="text-xs py-1 px-6 rounded-md bg-slate-300 text-slate-600 font-bold">Share: {{ data.postInfo.share_count }}</button>
+                    <TNowButton> Like: {{ randomInt(100) }} </TNowButton>
+                    <TNowButton> Comment: {{ randomInt(100) }} </TNowButton>
+                    <TNowButton> Share: {{ randomInt(100) }} </TNowButton>
                 </div>
             </div>
         </div>
