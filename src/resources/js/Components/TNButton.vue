6b76c2084b60fe12c75f424993efbf1ac09e5f72
juborajnaofel <juborajnaofel@gmail.com>
2023-06-16 05:57:32 +0600

    new button component

---
+++ src/resources/js/Components/TNButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+const classes =  ref("text-xs py-1 px-3 rounded-xl bg-slate-300 text-slate-600 font-bold");
+const props = defineProps({
+    variant: String,
+});
+</script>
+
+<template>
+    <button
+        :class="classes"
+    >
+        <slot/>
+    </button>
+</template>
