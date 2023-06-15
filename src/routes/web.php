b4da460cb13f59e2c766401d7fb8502d2537ae94
juborajnaofel <juborajnaofel@gmail.com>
2023-06-16 05:54:47 +0600

    post fetch route and route file refactored

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Homefeed\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -25,14 +26,18 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/home', function () {
-    return Inertia::render('Homefeed');
-})->middleware(['auth', 'verified'])->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/home', function () { return Inertia::render('Homefeed');})->name('home');
+    Route::get('/home/posts/fetch', [HomeController::class, 'fetchPosts'])->name('home.posts.fetch');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__ . '/auth.php';
