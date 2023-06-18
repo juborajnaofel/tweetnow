98ebf0dd58403d687e1182ef7dc0b7e18b1c56ef
juborajnaofel <juborajnaofel@gmail.com>
2023-06-18 23:57:30 +0600

    share redirect toast data

--- src/app/Http/Middleware/HandleInertiaRequests.php
+++ src/app/Http/Middleware/HandleInertiaRequests.php
@@ -39,6 +39,12 @@ class HandleInertiaRequests extends Middleware
                     'location' => $request->url(),
                 ]);
             },
+            'toast' => function () use ($request) {
+                return [
+                    'status' => $request->session()->get('status'),
+                    'message' => $request->session()->get('message'),
+                ];
+            },
         ]);
     }
 }
