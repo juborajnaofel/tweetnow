c61689d97953f1f66b3f66bfba68afe79ea7f428
juborajnaofel <juborajnaofel@gmail.com>
2023-06-18 23:58:27 +0600

    post box submit post

--- src/resources/js/Components/PostBox.vue
+++ src/resources/js/Components/PostBox.vue
@@ -1,9 +1,23 @@
 <script setup>
-import { useForm } from "@inertiajs/vue3";
+import { useForm, usePage } from "@inertiajs/vue3";
 
 const form = useForm({
-    post_field: "",
+    content: "",
 });
+
+const submitPost = (event) => {
+    event.preventDefault();
+    form.post(route("post.store"), {
+        onFinish() {
+            form.content = "";
+            const { toast } = usePage().props;
+            alert(toast.status + ": " + toast.message);
+        },
+        onError() {
+            alert("Error!");
+        },
+    });
+};
 </script>
 
 <template>
@@ -12,17 +26,18 @@ const form = useForm({
             class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
         >
             <div class="p-1 text-gray-900 dark:text-gray-100">
-                <form action="" method="post" class="w-full">
+                <form class="w-full">
                     <textarea
                         @change=""
                         name="posthere"
                         placeholder="What are you thinking?"
-                        v-model="form.post_field"
+                        v-model="form.content"
                         rows="5"
                         class="w-full rounded-md border-2 border-slate-100"
                     ></textarea>
                     <div class="flex flex-row justify-end">
                         <button
+                            @click="submitPost"
                             class="py-3 px-6 rounded-md bg-slate-300 text-slate-600 font-bold"
                         >
                             Post
