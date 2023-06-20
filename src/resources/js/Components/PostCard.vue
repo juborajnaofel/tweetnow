984f395d5c49510877ad71d50ec79e366554c1fd
juborajnaofel <juborajnaofel@gmail.com>
2023-06-20 23:29:03 +0600

    post card action menu, design improve

--- src/resources/js/Components/PostCard.vue
+++ src/resources/js/Components/PostCard.vue
@@ -1,6 +1,10 @@
 <script setup>
 import TNowButton from "@/Components/TNButton.vue";
 import moment from "moment";
+import { ref } from "vue";
+
+const menuShow = ref(false);
+
 const formattedDate = (datatime) => {
     return moment(datatime).format("MMMM Do YYYY, h:mm:ss a");
 };
@@ -15,19 +19,51 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="max-w-full mb-4 mx-auto px-2">
+    <div class="max-w-full mb-2 mx-auto mx-2">
+        <div class="relative">
+            <ul
+                class="absolute right-0 top-4 mt-2 w-20 bg-white rounded-md shadow-lg"
+                :class="menuShow ? '' : 'hidden'"
+            >
+                <li>
+                    <a href="#" class="block px-4 py-1 text-xs text-gray-800">
+                        Delete
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="block px-4 py-1 text-xs text-gray-800">
+                        Edit
+                    </a>
+                </li>
+            </ul>
+        </div>
+
         <div
-            class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
+            class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm rounded-2xl"
         >
-            <div class="p-5 text-gray-900 dark:text-gray-100">
-                <span class="font-bold">{{ data.user.name }}</span>
-                <br /><span>{{ formattedDate(data.created_at) }}</span>
+            <div class="text-gray-900 dark:text-gray-100">
+                <div class="flex flex-row justify-between px-1">
+                    <div class="flex flex-col p-2">
+                        <span class="font-bold">{{ data.user.name }}</span>
+                        <span class="text-xs">
+                            {{ formattedDate(data.created_at) }}
+                        </span>
+                    </div>
+
+                    <span
+                        @click="menuShow = !menuShow"
+                        class="text-gray-800 text-xs font-semibold select-none cursor-pointer px-3"
+                    >
+                        ...
+                    </span>
+                </div>
+
                 <!-- <span class="text-xs p-2" v-if="data.user.name">✔</span> -->
-                <p class="py-5">
-                    {{ data.content }}
+                <p class="pt-2 pb-3 px-3 text-sm">
+                    {{ data.content }} {{ data.post_permission }}
                 </p>
                 <hr />
-                <div class="flex flex-row flex-wrap gap-3 pt-3">
+                <div class="flex flex-row flex-wrap gap-3 p-1">
                     <TNowButton> Like: {{ randomInt(100) }} </TNowButton>
                     <TNowButton> Comment: {{ randomInt(100) }} </TNowButton>
                     <TNowButton> Share: {{ randomInt(100) }} </TNowButton>
