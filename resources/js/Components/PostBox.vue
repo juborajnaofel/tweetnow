c38ca510bf207d52042b13125621170315bd337f
juborajnaofel <juborajnaofel@gmail.com>
2024-07-25 18:20:07 +0600

    component updated design and function

--- resources/js/Components/PostBox.vue
+++ resources/js/Components/PostBox.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { useForm, usePage } from "@inertiajs/vue3";
-import { ref } from "vue";
+import { useForm, usePage } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 const form = useForm({
-    content: "",
+    content: '',
 });
+const selectedTab = ref('tab1');
 
 const allowedLength = ref(usePage().props.configs.post_character_limit);
 
@@ -17,21 +18,21 @@ const submitPost = (event) => {
 
     if (isContentCharNumberNotAllowed()) {
         alert(
-            "Only " +
+            'Only ' +
                 allowedLength.value +
-                " characters are allowed for your post!"
+                ' characters are allowed for your post!'
         );
         return;
     }
 
-    form.post(route("post.store"), {
+    form.post(route('post.store'), {
         onFinish() {
-            form.content = "";
+            form.content = '';
             const { toast } = usePage().props;
-            alert(toast.status + ": " + toast.message);
+            alert(toast.status + ': ' + toast.message);
         },
         onError() {
-            alert("Error!");
+            alert('Error!');
         },
     });
 };
@@ -39,39 +40,38 @@ const submitPost = (event) => {
 
 <template>
     <div class="w-full mx-auto px-2">
-        <div
-            class="bg-slate-100 border-2 border-slate-200 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
-        >
-            <div class="p-1 text-gray-900 dark:text-gray-100">
-                <form class="w-full">
+        <div class="bg-gray-900 rounded-lg shadow-md overflow-hidden">
+            <div class="flex">
+                <!-- Radio inputs -->
+                <input type="radio" id="tab1" name="tabs" v-model="selectedTab" value="tab1" class="hidden">
+                <label for="tab1" class="border border-gray-600 py-2 px-4 bg-gray-800 cursor-pointer text-gray-300 hover:bg-gray-700">Story</label>
+
+                <input type="radio" id="tab2" name="tabs" v-model="selectedTab" value="tab2" class="hidden">
+                <label for="tab2" class="border border-gray-600 py-2 px-4 bg-gray-800 cursor-pointer text-gray-300 hover:bg-gray-700">Post</label>
+            </div>
+            <div class="p-4 text-white">
+                <form>
                     <textarea
                         name="posthere"
                         placeholder="What are you thinking?"
                         v-model="form.content"
                         rows="2"
-                        class="w-full text-sm rounded-md border-none bg-slate-200 focus:outline-none"
+                        class="w-full text-sm rounded-md bg-gray-800 focus:outline-none py-2 px-3 placeholder-gray-500"
                     ></textarea>
-                    <div class="flex flex-row justify-between">
-                        <div>
-                            <span
-                                class="font-bold text-[10px]"
-                                :class="
-                                    isContentCharNumberNotAllowed()
-                                        ? 'text-red-400'
-                                        : ''
-                                "
-                            >
-                                <template v-if="form.content.length > 0">
-                                    {{ form.content.length }}/
-                                    {{
-                                        allowedLength
-                                    }}
-                                </template>
-                            </span>
-                        </div>
+                    <div class="flex justify-between items-center mt-2">
+                        <span
+                            :class="{
+                                'text-red-400': isContentCharNumberNotAllowed(),
+                                'text-gray-400':
+                                    !isContentCharNumberNotAllowed(),
+                            }"
+                            class="text-xs font-semibold"
+                        >
+                            {{ form.content.length }} / {{ allowedLength }}
+                        </span>
                         <button
                             @click="submitPost"
-                            class="py-1 px-4 text-sm rounded-md bg-slate-300 text-slate-600 font-bold"
+                            class="py-2 px-4 text-sm rounded-md bg-blue-500 text-white font-bold hover:bg-blue-600 focus:outline-none"
                         >
                             Post
                         </button>
