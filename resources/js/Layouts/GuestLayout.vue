b8e417a3db0f4101825ed70fe8101143ac99ecd7
juborajnaofel <juborajnaofel@gmail.com>
2024-07-25 18:17:51 +0600

    Guest Layout refactor, design improve

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,17 +4,30 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-darktech-dark">
         <div>
             <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <ApplicationLogo class="w-20 h-20 fill-current text-darktech-primary" />
             </Link>
         </div>
 
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
-        >
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-darktech-light shadow-md dark:bg-darktech-dark sm:rounded-lg">
             <slot />
         </div>
     </div>
 </template>
+
+<style scoped>
+.bg-darktech-dark {
+    background-color: #36393f;
+    color: #ffffff;
+}
+
+.bg-darktech-light {
+    background-color: #ffffff;
+}
+
+.text-darktech-primary {
+    color: #7289da;
+}
+</style>
